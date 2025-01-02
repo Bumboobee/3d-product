e7a1d64181558feb61401f78cc706011374764bd
Jose Aparecido <juniorsilva240803@gmail.com>
2025-01-02 10:59:45 -0300

    fix: go back to the render (😅)

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,14 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-app.options("*", cors());
-
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: `Welcome to the ${process.env.APP_NAME} API!` });
 });
