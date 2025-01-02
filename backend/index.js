8ca31bd960ae5c6d83487c882c83b27f76dbab78
Jose Aparecido <juniorsilva240803@gmail.com>
2025-01-02 10:23:04 -0300

    feat: Vercel configuration and favicon added

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,23 @@
 import cors from "cors";
+import path from "path";
 import express from "express";
 import * as dotenv from "dotenv";
+import favicon from "serve-favicon";
 import dalleRoutes from "./routes/dalle.routes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(favicon(path.join(path.resolve(), "public", "favicon.ico")));
+
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World" });
+  res.status(200).json({ message: `Welcome to the ${process.env.APP_NAME} API!` });
 });
 
 app.listen(process.env.PORT, () => {
