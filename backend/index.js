5bec4285d4b3a1863c708819d3b5222c6de0e4d2
Jose Aparecido <juniorsilva240803@gmail.com>
2025-01-02 10:44:26 -0300

    feat: allow all origins in the cors

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,8 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
+app.options("*", cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
